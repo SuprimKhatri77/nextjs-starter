da9db54977070d9b433d17fa6e804c7ce2a961e4
Suprim Khatri <Supremekhatri62@gmail.com>
2026-03-05 10:02:01 +0545

    chore: removed unwanted dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="bg-gradient-to-b ">Hello world!</h1>
+      <h1 className="bg-linear-to-b ">Hello world!</h1>
     </div>
   );
 }
--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -1,7 +1,7 @@
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db";
 import * as schema from "@/db/schema";
 import { betterAuth } from "better-auth";
-import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
@@ -9,7 +9,7 @@ export const auth = betterAuth({
     provider: "pg",
     schema,
   }),
-  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3000",
+  baseURL: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
   emailAndPassword: {
     enabled: true,
   },
